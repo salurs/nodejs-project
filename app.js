0af6f292c392662e4d8cef58cb01cdc8f7ae4b65
orhansalur <orhan.salur@teknofix.com.tr>
2021-11-28 22:47:53 +0300

    static files added

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-const fn = function deneme(params) {
-    console.log('test');
-};
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//static files
+app.use(express.static('public'));
+app.use(express.static('template'));
+
+app.get('/', function (req, res) {
+    const photo = {
+        id: 1,
+        name: 'Photo name',
+        description: 'Photo description',
+    };
+    res.status(200).send(photo);
+});
+
+app.get('/index', function (req, res) {
+    res.sendFile(path.resolve(__dirname,'template/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(__dirname);
+    console.log(`Server start on ${port} port.`);
+});
